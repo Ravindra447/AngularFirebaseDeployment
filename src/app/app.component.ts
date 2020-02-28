b4bcd1f8a101efa602476b4b344c87fe375b5ad2
Ravindra447 <ravindra.ch447@gmail.com>
2020-02-28 01:32:47 -0500

    initial app with firebase deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularFirebase';
+  firebaseConfig = {
+    apiKey: "AIzaSyBVfEESE5-Q6q3WMNe1seN-ulACtaTihMM",
+    authDomain: "angularfirebase-3e93c.firebaseapp.com",
+    databaseURL: "https://angularfirebase-3e93c.firebaseio.com",
+    projectId: "angularfirebase-3e93c",
+    storageBucket: "angularfirebase-3e93c.appspot.com",
+    messagingSenderId: "430867444007",
+    appId: "1:430867444007:web:b793083eb45c04bbe4b902",
+    measurementId: "G-T7J58G27S2"
+  };
 }
